ba86647a23d1f48e7e59d318a562d6ed3db5fe21
Thiago099 <tecnologiasthiago@gmail.com>
2022-07-14 18:26:25 -0300

    first commit

---
+++ ProcessWatcher/ProcessWatcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+namespace ProcessUtils
+{
+    public class ProcessWatcher
+    {
+        private Process process;
+        public ProcessWatcher(string file, DataReceivedEventHandler callback = null, string arguments = null)
+        {
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = file,
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            process = new Process()
+            {
+                StartInfo = processStartInfo
+            };
+            process.OutputDataReceived += callback;
+            process.Start();
+            process.BeginOutputReadLine();
+        }
+        public void WaitForExit()
+        {
+            process.WaitForExit();
+        }
+    }
+}
---
+++ watcher/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using ProcessUtils;
+
+
+var p1 = new ProcessWatcher(
+    "watch-me.exe",
+    (sender, args) => Console.WriteLine("received output: {0}", args.Data)
+);
+var p2 = new ProcessWatcher(
+    "watch-me.exe",
+    (sender, args) => Console.WriteLine("received output: {0}", args.Data)
+);
+
+p1.WaitForExit();
+p2.WaitForExit();
